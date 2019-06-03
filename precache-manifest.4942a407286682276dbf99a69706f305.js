ea5dfe734cf503485abb8412b95ee77b68d36f90
Xleine <919143384@qq.com>
2019-06-03 15:28:04 +0800

    Updates

---
+++ precache-manifest.4942a407286682276dbf99a69706f305.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83077a5fa341a40cb8d53f7d7a8fdb02",
+    "url": "/shuang-pin-react/index.html"
+  },
+  {
+    "revision": "b88a07923e2bb4cda4d5",
+    "url": "/shuang-pin-react/static/css/main.714b79c7.chunk.css"
+  },
+  {
+    "revision": "caa9a7fa08bf2c0d73b8",
+    "url": "/shuang-pin-react/static/js/2.f0910273.chunk.js"
+  },
+  {
+    "revision": "b88a07923e2bb4cda4d5",
+    "url": "/shuang-pin-react/static/js/main.bd96c241.chunk.js"
+  },
+  {
+    "revision": "330ed018a903ed46cc56",
+    "url": "/shuang-pin-react/static/js/runtime~main.e241b99a.js"
+  },
+  {
+    "revision": "dafe415cfdbaedfdaa8de6f71a550617",
+    "url": "/shuang-pin-react/static/media/xiaohe.dafe415c.png"
+  }
+]);
